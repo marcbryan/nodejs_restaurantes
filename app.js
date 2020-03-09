c35601401422bbdfc427028badf8d8c09136bf37
marcbryan <marcboakyeflores98@gmail.com>
2020-03-09 18:00:49 +0100

    Form categories

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 require('dotenv').config();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 var mongo = require('mongodb').MongoClient;
 var mongoClient;
+var ObjectID = require('mongodb').ObjectID;
 
 // constantes
 const PORT = process.env.PORT || 3000;
@@ -43,17 +44,51 @@ app.get('/restaurantes/create', function(req, res) {
 
 app.post('/restaurantes/create', function(req, res) {
   var name = req.body.name;
-  var address = req.body.address;
-  var phone = req.body.phone;
   var email = req.body.email;
+  var phone = req.body.phone;
+  var city = req.body.city;
   var zipcode = req.body.zipcode;
+  var address = req.body.address;
 
-  var rest = {name:name, address:address, phone:phone, email:email, zipcode:zipcode};
+  var rest = {name:name, email:email, phone:phone, city:city, zipcode:zipcode, address:address, coord:{lat:0.0, lon:0.0}, schedule:[], categories:[], products:[]};
 
   var db = mongoClient.db('mongo');
   db.collection('restaurantes').insertOne(rest, function(err, records) {
     if (err) throw err;
     console.log("Inserted: "+records.insertedCount);
-    res.send('ok');
+    res.redirect('/restaurantes');
   });
+});
+
+app.get('/categories/create', function(req, res) {
+  res.render('newCategory');
+});
+
+app.get('/restaurantes/:id/updateCategories', function(req, res) {
+  var db = mongoClient.db('mongo');
+  var options = {};
+  var query = {_id: new ObjectID(req.params.id)};
+  console.log(query);
+  db.collection('restaurantes').findOne(query, options, function(err, doc) {
+    if (err) {
+      res.render('error', {msg:'Error en la query!'});
+      return;
+    }
+    if (doc.length == 0) {
+      res.redirect('/restaurantes');
+    }
+    console.log(doc.categories);
+    res.render('editCategories', {categories: doc.categories});
+  });
+});
+
+app.post('/restaurantes/:id/updateCategories', function(req, res) {
+  var id = req.params.id;
+  var db = mongoClient.db('mongo');
+  console.log(req.body.category);
+  db.collection('restaurantes').updateOne(
+    { '_id':  new ObjectID(id) },
+    { $push: {'categories':{$each: req.body.category}} }
+  );
+  res.redirect('/restaurantes/'+id+'/updateCategories');
 });
